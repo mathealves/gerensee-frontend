833df7dc6e960824049207bf1b97deca0986a9d6
Matheus S Alves <matheus.souza.alves@gmail.com>
2026-03-08 11:58:31 -0300

    fix: update version label in Footer component from Beta to Alpha

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <section>
           <span>Gerensee</span>
           <span className="inline-flex items-center bg-slate-100/90 dark:bg-slate-800/90 text-slate-800 dark:text-slate-100 rounded-full px-1.5 py-0.5 font-semibold text-xs ml-0.5">
-            Beta
+            Alpha
           </span>
         </section>
         <span>© {year} Matheus S. Alves</span>
