6bc20fa626c817d3661bc7eef0d135afacdbe56f
Matheus S Alves <matheus.souza.alves@gmail.com>
2026-03-08 01:02:07 -0300

    fix: update ESLint configuration to use recommended Prettier plugin and add it as a dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import prettier from "eslint-config-prettier";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  prettier,
+  prettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
